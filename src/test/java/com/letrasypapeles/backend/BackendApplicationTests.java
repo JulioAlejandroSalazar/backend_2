45c3d673625fe16d50ae8be40860745d3645db01
Julio <julio522002@gmail.com>
2025-06-12 22:20:54 -0400

    test upt

--- src/test/java/com/letrasypapeles/backend/BackendApplicationTests.java
+++ src/test/java/com/letrasypapeles/backend/BackendApplicationTests.java
@@ -3,12 +3,16 @@ package com.letrasypapeles.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
 class BackendApplicationTests {
+
     @Test
     void contextLoads() {
     }
-}
 
+}
